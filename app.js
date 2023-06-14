e0dcb349fad405586b2087182320cb33e25953fc
nursultanza <105927974+nursultanza@users.noreply.github.com>
2023-06-15 03:23:03 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+document.addEventListener("DOMContentLoaded", function() {
+  let slideIndex = 1;
+  showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+  function showSlides(n) {
     let i;
     const slides = document.getElementsByClassName("mySlides");
-    const dots = document.getElementsByClassName("dot");
-  
+
     if (n > slides.length) {
       slideIndex = 1;
     }
     if (n < 1) {
       slideIndex = slides.length;
     }
-  
+
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
+      slides[i].classList.remove("fade-in"); // Remove the "fade-in" class
     }
-  
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-  
+
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    slides[slideIndex - 1].classList.add("fade-in"); // Add the "fade-in" class
   }
+
+  const prevButton = document.querySelector(".prev");
+  const nextButton = document.querySelector(".next");
+
+  prevButton.addEventListener("click", function() {
+    plusSlides(-1);
+  });
+
+  nextButton.addEventListener("click", function() {
+    plusSlides(1);
+  });
+
+  setInterval(function() {
+    plusSlides(1);
+  }, 5000);
+});
